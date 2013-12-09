d6354030ae20a584832e68c7a7879fe36089107a
Thomas Gläßle <t_glaessle@gmx.de>
2013-12-10 00:16:13 +0100

    Make files executable

--- glycolysis.m
+++ glycolysis.m
@@ -1,5 +1,5 @@
 function x = glycolysis(x0, y0, a, b, dt=0.1, tmax=30)
-    xinit = [x0, p0];
+    xinit = [x0, y0];
     t = linspace(0, tmax, tmax / dt);
 
     xdot = @(x,t) [a*x(2) + x(2)*x(1)**2 - x(1),
--- glycosolve.m
+++ glycosolve.m
@@ -4,7 +4,7 @@ a = str2double(getenv("a"));
 b = str2double(getenv("b"));
 dt = str2double(getenv("dt"));
 tf = str2double(getenv("tf"));
-p0 = str2double(getenv("p0"));
+y0 = str2double(getenv("y0"));
 
-disp(glycolysis(0, p0, a, b, dt, tf));
+disp(glycolysis(0, y0, a, b, dt, tf));
 
