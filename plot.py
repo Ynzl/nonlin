45356f79bde0581f3473c017f96d289667360122
Thomas Gläßle <t_glaessle@gmx.de>
2013-12-09 22:39:10 +0100

    Cosmetic surgery

--- plot.py
+++ plot.py
@@ -16,21 +16,21 @@ def call(*args, **kwargs):
 os.makedirs('graph')
 
 for dt,eps in product((0.1,0.01,0.001), (0,0.1,5)):
-    print("dt=%f, eps=%f" % (dt,eps))
-    euler = call('bin/euler', dt=dt, eps=eps, p0=0.1)
-    rk4 = call('bin/rk4', dt=dt, eps=eps, p0=0.1)
+    print("dt=%s, eps=%s" % (dt,eps))
+    euler = call('bin/euler', dt=dt, eps=eps, p0=0.1, tf=150)
+    rk4 = call('bin/rk4', dt=dt, eps=eps, p0=0.1, tf=150)
     euler = np.loadtxt(StringIO(euler))
     rk4 = np.loadtxt(StringIO(rk4))
 
-    print("plotting...")
+    # plot x(t), p(t)
     figure = plt.figure()
     axes = figure.add_subplot(111)
-    figure.suptitle(r'$\Delta t=%f$, $\epsilon=%f$' % (dt, eps))
+    figure.suptitle(r'$\Delta t=%s$, $\epsilon=%s$' % (dt, eps))
 
-    axes.plot(euler[:,0], euler[:,1], label=r'$x_{\mathrm{eul}}$')
-    axes.plot(euler[:,0], euler[:,2], label=r'$\dot{x}_{\mathrm{eul}}$')
-    axes.plot(rk4[:,0], rk4[:,1], label=r'$x_{\mathrm{rk4}}$')
-    axes.plot(rk4[:,0], rk4[:,2], label=r'$\dot{x}_{\mathrm{rk4}}$')
+    axes.plot(euler[:,0], euler[:,1], label=r'$x_\mathrm{eul}$')
+    axes.plot(euler[:,0], euler[:,2], label=r'$\dot{x}_\mathrm{eul}$')
+    axes.plot(rk4[:,0], rk4[:,1], label=r'$x_\mathrm{rk4}$')
+    axes.plot(rk4[:,0], rk4[:,2], label=r'$\dot{x}_\mathrm{rk4}$')
 
     axes.legend(loc='lower left')
     figure.savefig("graph/time-evolution_dt=%s_eps=%s.png"%(dt,eps))
