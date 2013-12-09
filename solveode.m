ac25abdeda88eb06f176fb55432d8ab6281e48be
Yuyan Li <yuyan.li@gmx.net>
2013-12-09 23:35:30 +0100

    octave stuff

--- solveode.m
+++ solveode.m
@@ -1,6 +1,6 @@
-function x = solveode(eps, dt=0.1, tmax=30)
+function x = solveode(eps, dt=0.1, tmax=20)
     xinit = [0, 0.1];
-    t = linspace(0, tmax, tmax / dt);
+    t = linspace(0, tmax, tmax / dt + 1)';
 
     xdot = @(x,t) [x(2), eps * (1 - x(1)**2) * x(2) - x(1)];
 
